33acf0677a7372adb5944b7b07a42bdb9443ac61
Vik <v.kazancev17@gmail.com>
2024-08-16 15:44:48 +0300

    Make cat.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanSecondHW =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanFirstCat = (Cat) applicationContext.getBean("cat");
+        Cat beanSecondCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == beanSecondHW);
+        System.out.println(beanFirstCat == beanSecondCat);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,13 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat makeCat() {
+        Cat cat = new Cat();
+        cat.setName("Nameless Cat");
+        return cat;
+    }
+
 }
